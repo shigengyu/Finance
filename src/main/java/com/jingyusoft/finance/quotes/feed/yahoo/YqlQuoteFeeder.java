a23817bb662a8aa331ee9230178823661593f93a
Gengyu Shi <univer.shi@gmail.com>
2014-08-05 23:19:37 +0100

    Remove unused classes

--- src/main/java/com/jingyusoft/finance/quotes/feed/yahoo/YqlQuoteFeeder.java
+++ src/main/java/com/jingyusoft/finance/quotes/feed/yahoo/YqlQuoteFeeder.java
@@ -3,20 +3,37 @@ package com.jingyusoft.finance.quotes.feed.yahoo;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Field;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
 
+import com.google.common.collect.Lists;
 import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
 import com.jingyusoft.finance.quotes.feed.QuoteFeeder;
+import com.jingyusoft.finance.quotes.feed.yahoo.dto.YqlJsonQuote;
 import com.jingyusoft.finance.quotes.feed.yahoo.dto.YqlJsonResponse;
 
 @Service
 public class YqlQuoteFeeder implements QuoteFeeder {
+
+	private static List<String> getColumns(Class<?> clazz) {
+		List<String> columns = Lists.newArrayList();
+		for (Field field : clazz.getDeclaredFields()) {
+			SerializedName serializedName = field.getAnnotation(SerializedName.class);
+			if (serializedName != null) {
+				columns.add(serializedName.value());
+			}
+		}
+		return columns;
+	}
 
 	public static void main(String[] args) throws IOException {
 
@@ -37,21 +54,19 @@ public class YqlQuoteFeeder implements QuoteFeeder {
 
 	public void test() throws IOException {
 
-		String query = "select * from yahoo.finance.quotes where symbol in ('MS', 'GS')";
-		String fullUrlStr = baseUrl + "q=" + URLEncoder.encode(query, "UTF-8")
-				+ "&format=" + format + "&env=" + env;
+		final String columns = StringUtils.join(getColumns(YqlJsonQuote.class), ",");
+		String query = "select " + columns + " from yahoo.finance.quotes where symbol in ('MS', 'GS')";
+		String fullUrlStr = baseUrl + "q=" + URLEncoder.encode(query, "UTF-8") + "&format=" + format + "&env=" + env;
 
 		System.out.println(fullUrlStr);
 
 		URL fullUrl = new URL(fullUrlStr);
-		try (BufferedReader in = new BufferedReader(new InputStreamReader(
-				fullUrl.openStream()))) {
+		try (BufferedReader in = new BufferedReader(new InputStreamReader(fullUrl.openStream()))) {
 
 			Gson gson = new Gson();
 			YqlJsonResponse response = gson.fromJson(in, YqlJsonResponse.class);
 			System.out.println(response.getQuery().getCount());
-			System.out.println(response.getQuery().getResults().getQuotes()
-					.get(0).getSymbol());
+			System.out.println(response.getQuery().getResults().getQuotes().get(0).getSymbol());
 		}
 	}
 }
